07f3f5c0d491303fbf7e4d85112f0195fa938556
Pranu77710 <pranshulmankad09@gmail.com>
2022-09-07 21:26:07 +0530

    Second Commit

--- 1.py
+++ 1.py
@@ -1,3 +1,15 @@
 a=int(input("Enter The First Number: "))
 b=int(input("Enter The Second Number: "))
-print("The sum of the numbers", a, "and", b, "is", a+b, "\b.")
+print("The sum of the numbers", a, "and", b, "gives", a+b, "\b.")
+
+a=int(input("Enter The First Number: "))
+b=int(input("Enter The Second Number: "))
+print("The subtraction of the numbers", a, "and", b, "gives", a-b, "\b.")
+
+a=int(input("Enter The First Number: "))
+b=int(input("Enter The Second Number: "))
+print("The product of the numbers", a, "and", b, "gives", a*b, "\b.")
+
+a=int(input("Enter The First Number: "))
+b=int(input("Enter The Second Number: "))
+print("The division of the numbers", a, "and", b, "gives", a/b, "\b.")
