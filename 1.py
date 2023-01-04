b0a44274eabfa268409d9211ec3d2ee0f4dccc8b
Pranu77710 <113044831+Pranu77710@users.noreply.github.com>
2023-01-04 13:24:33 +0530

    third
    hi hello

--- 1.py
+++ 1.py
@@ -6,6 +6,8 @@ a=int(input("Enter The First Number: "))
 b=int(input("Enter The Second Number: "))
 print("The subtraction of the numbers", a, "and", b, "gives", a-b, "\b.")
 
+hi hello
+
 a=int(input("Enter The First Number: "))
 b=int(input("Enter The Second Number: "))
 print("The product of the numbers", a, "and", b, "gives", a*b, "\b.")
